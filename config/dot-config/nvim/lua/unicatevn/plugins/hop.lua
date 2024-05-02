b6eefd3d997442d28898187011d4299c0f9f91b0
Unicatevn <tuananh.vga@gmail.com>
2024-05-02 09:34:36 +0700

    optimize hop keymap

--- config/dot-config/nvim/lua/unicatevn/plugins/hop.lua
+++ config/dot-config/nvim/lua/unicatevn/plugins/hop.lua
@@ -4,13 +4,13 @@ return {
   config = function(_)
     local hop = require('hop')
     hop.setup { keys = 'etovxqpdygfblzhckisuran' }
-    vim.keymap.set('', '<leader><leader>w', function()
+    vim.keymap.set('', '<leader>w', function()
       hop.hint_words({ current_line_only = false })
     end, { remap = true, desc = 'Hop: Words' })
-    vim.keymap.set('n', '<leader><leader>s', function()
+    vim.keymap.set('n', '<leader>s', function()
       hop.hint_char1({ current_line_only = false })
     end, { remap = false, desc = 'Hop: Chars' })
-    vim.keymap.set('v', '<leader><leader>s', function()
+    vim.keymap.set('v', '<leader>s', function()
       hop.hint_char1({ current_line_only = false })
     end, { remap = false, desc = 'Hop: Chars Visual' })
   end
