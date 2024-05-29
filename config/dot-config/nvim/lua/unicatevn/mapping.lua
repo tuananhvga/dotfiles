a083effdc48551fb9f28c9700cffebdfa80df5ac
Unicatevn <tuananh.vga@gmail.com>
2024-05-29 16:21:32 +0700

    add fugitive mapping

--- config/dot-config/nvim/lua/unicatevn/mapping.lua
+++ config/dot-config/nvim/lua/unicatevn/mapping.lua
@@ -9,5 +9,24 @@ vim.api.nvim_set_keymap('n', ']q', '<cmd>cn<CR>',
   { noremap = true, silent = true, desc = "Go to previous quickfix item" })
 vim.api.nvim_set_keymap('n', '[q', '<cmd>cp<CR>', { noremap = true, silent = true, desc = "Go to next quickfix item" })
 
+-- Diff keymap
+vim.api.nvim_set_keymap('n', '<C-g>', '<cmd>Gvdiffsplit!<CR>', { noremap = true, silent = true, desc = "Three ways git diff" })
+vim.keymap.set('v', '<leader>dp', function ()
+  vim.cmd('diffput')
+  vim.cmd('diffupdate')
+end, { desc = "Diff put" })
+vim.keymap.set('v', '<leader>dg', function ()
+  vim.cmd('diffget')
+  vim.cmd('diffupdate')
+end, { desc = "Diff get" })
+vim.keymap.set('v', '<leader>dt', function ()
+  vim.cmd('diffget //2')
+  vim.cmd('diffupdate')
+end, { desc = "Diff get target branch" })
+vim.keymap.set('v', '<leader>dy', function ()
+  vim.cmd('diffget //3')
+  vim.cmd('diffupdate')
+end, { desc = "Diff get merge branch" })
+
 -- Open tmux sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux_sessionizer<CR>", { desc = "Open tmux sessionizer" })
