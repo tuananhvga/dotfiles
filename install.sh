4517324b83ff3fb2ba723a2d2eab1488a55e7175
Unicatevn <tuananh.vga@gmail.com>
2024-06-15 09:02:39 +0700

    reorganize stow

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ cd ~/dotfiles/ansible
 sudo ansible-playbook sudo.yaml
 ansible-playbook home.yaml
 cd ~/dotfiles
+mkdir -p ~/.config
 stow -v --dotfiles -d config -t ~ .
+stow -v --dotfiles -d home -t ~ .
