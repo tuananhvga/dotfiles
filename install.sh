a9f2f2a078dfc79eea133341adbd1344956be99b
Unicatevn <tuananh.vga@gmail.com>
2024-07-03 22:02:06 +0700

    fix: stow config dir

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo ansible-playbook sudo.yaml
 ansible-playbook home.yaml
 cd ~/dotfiles
 mkdir -p ~/.config
-stow -v --dotfiles -d config -t ~ .
+stow -v --dotfiles -d config/dot-config -t ~/.config .
 stow -v --dotfiles -d home -t ~ .
