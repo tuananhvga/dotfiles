a6111649108f0a8015dbdbbae75a1856a102e022
Unicatevn <tuananh.vga@gmail.com>
2024-09-05 16:28:20 +0700

    feat: update ensure installed

--- config/dot-config/nvim/lua/unicatevn/plugins/lsp.lua
+++ config/dot-config/nvim/lua/unicatevn/plugins/lsp.lua
@@ -51,7 +51,16 @@ return {
     require('mason').setup({})
     require('mason-lspconfig').setup({
       ensure_installed = {
-        'lua_ls', 'clangd', 'bashls', 'tsserver', 'bufls', 'html', 'eslint', 'gopls'
+        'lua_ls',
+        'clangd',
+        'bashls',
+        'tsserver',
+        'bufls',
+        'html',
+        'eslint',
+        'gopls',
+        'cssls',
+        'emmet_language_server',
       },
       handlers = {
         default_setup,
@@ -74,6 +83,32 @@ return {
           }
         }
       }
+    })
+
+    require('lspconfig').emmet_language_server.setup({
+      filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact", "templ" },
+      -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+      -- **Note:** only the options listed in the table are supported.
+      init_options = {
+        ---@type table<string, string>
+        includeLanguages = {},
+        --- @type string[]
+        excludeLanguages = {},
+        --- @type string[]
+        extensionsPath = {},
+        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+        preferences = {},
+        --- @type boolean Defaults to `true`
+        showAbbreviationSuggestions = true,
+        --- @type "always" | "never" Defaults to `"always"`
+        showExpandedAbbreviation = "always",
+        --- @type boolean Defaults to `false`
+        showSuggestionsAsSnippets = false,
+        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+        syntaxProfiles = {},
+        --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+        variables = {},
+      },
     })
 
     local cmp = require('cmp')
