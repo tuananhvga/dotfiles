0f5163ee5862dd98e4b03221f98c2ecc97ef0fd4
Unicatevn <tuananh.vga@gmail.com>
2024-10-04 09:18:49 +0700

    feat: open new tmux windows of current file directory

--- config/dot-config/nvim/lua/unicatevn/mapping.lua
+++ config/dot-config/nvim/lua/unicatevn/mapping.lua
@@ -7,33 +7,39 @@ vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true
 vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg_', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', ']q', '<cmd>cn<CR>',
   { noremap = true, silent = true, desc = "Go to next quickfix item" })
-vim.api.nvim_set_keymap('n', '[q', '<cmd>cp<CR>', { noremap = true, silent = true, desc = "Go to previous quickfix item" })
+vim.api.nvim_set_keymap('n', '[q', '<cmd>cp<CR>',
+  { noremap = true, silent = true, desc = "Go to previous quickfix item" })
 vim.api.nvim_set_keymap('n', ']b', '<cmd>bn<CR>',
   { noremap = true, silent = true, desc = "Go to next buffer" })
 vim.api.nvim_set_keymap('n', '[b', '<cmd>bp<CR>', { noremap = true, silent = true, desc = "Go to previous buffer" })
 
 -- Diff keymap
 vim.api.nvim_set_keymap('n', '<leader>gw', '<cmd>Gwrite<CR>', { noremap = true, silent = true, desc = "Git write" })
-vim.api.nvim_set_keymap('n', '<C-g>', '<cmd>Gvdiffsplit!<CR>', { noremap = true, silent = true, desc = "Three ways git diff" })
-vim.keymap.set('n', '<leader>dp', function ()
+vim.api.nvim_set_keymap('n', '<C-g>', '<cmd>Gvdiffsplit!<CR>',
+  { noremap = true, silent = true, desc = "Three ways git diff" })
+vim.keymap.set('n', '<leader>dp', function()
   vim.cmd('diffput')
   vim.cmd('diffupdate')
 end, { desc = "Diff put" })
-vim.keymap.set('n', '<leader>dg', function ()
+vim.keymap.set('n', '<leader>dg', function()
   vim.cmd('diffget')
   vim.cmd('diffupdate')
 end, { desc = "Diff get" })
-vim.keymap.set('n', '<leader>df', function ()
+vim.keymap.set('n', '<leader>df', function()
   vim.cmd('diffget //2')
   vim.cmd('diffupdate')
 end, { desc = "Diff get target branch" })
-vim.keymap.set('n', '<leader>dj', function ()
+vim.keymap.set('n', '<leader>dj', function()
   vim.cmd('diffget //3')
   vim.cmd('diffupdate')
 end, { desc = "Diff get merge branch" })
 
-vim.keymap.set('n', '<leader>py', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end, {desc = 'Copy file path'} )
-vim.keymap.set('n', '<leader>dy', function() vim.fn.setreg('+', vim.fn.expand('%:p:h')) end, {desc = 'Copy directory path'} )
+vim.keymap.set('n', '<leader>py', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end, { desc = 'Copy file path' })
+vim.keymap.set('n', '<leader>dy', function() vim.fn.setreg('+', vim.fn.expand('%:p:h')) end,
+  { desc = 'Copy directory path' })
 
 -- Open tmux sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux_sessionizer<CR>", { desc = "Open tmux sessionizer" })
+vim.keymap.set("n", "<leader>t", function()
+  vim.cmd("silent !tmux neww -c " .. vim.fn.expand('%:p:h'))
+end, { desc = "Open new shell at current file's directory" })
