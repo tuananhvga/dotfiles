c77f6df6b38e03a48940dd1265d3717a5af25f99
Unicatevn <tuananh.vga@gmail.com>
2025-05-02 16:47:58 +0700

    fix: skip confirm install and remove zshrc before linking

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-sudo pacman -Sy ansible ansible-core
+sudo pacman -Sy --no-confirm ansible ansible-core
 cd ~/dotfiles/ansible
 sudo ansible-playbook sudo.yaml
 ansible-playbook home.yaml
 cd ~/dotfiles
 mkdir -p ~/.config
+mkdir -p ~/dev/personal ~/dev/public
 stow -v --dotfiles -d config/dot-config -t ~/.config .
+rm -f ~/.zshrc
 stow -v --dotfiles -d home -t ~ .
